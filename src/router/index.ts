2c476ed50bbeae88c1b2de274eb1ad1223c12627
cesswhite <acessloop@gmail.com>
2021-08-19 17:16:09 -0500

    Fix index route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,8 @@
-import { RouteRecordRaw, createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import routes from "virtual:generated-pages"; // Routes generated automatically, system similar to nuxtjs
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
 });
-console.table("ROUTES", routes);
 export default router;
-
-//Examples
-
-// import type { RouteRecordRaw } from "vue-router";
-
-// const router = createRouter({
-//   history: createWebHashHistory(),
-//   routes: basicRoutes as RouteRecordRaw[],
-// });
